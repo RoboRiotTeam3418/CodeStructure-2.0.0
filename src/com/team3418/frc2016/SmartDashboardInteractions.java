96e4e7163674446a1744c65ad871e818c05961c1
nawage <nlwnawage@gmail.com>
2017-01-09 12:11:48 -0600

    fix

---
+++ src/com/team3418/frc2016/SmartDashboardInteractions.java
@@ -0,0 +1,8 @@
+package com.team3418.frc2016;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class SmartDashboardInteractions {
+
+}
